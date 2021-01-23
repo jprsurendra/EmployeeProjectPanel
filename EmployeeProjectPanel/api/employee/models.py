09a5a61c7fe42e3b2902fa074d0c97f175c798ec
jprsurendra <jpr_surendra@yahoo.co.in>
2021-01-23 11:52:01 +0530

    add employee model

--- EmployeeProjectPanel/api/employee/models.py
+++ EmployeeProjectPanel/api/employee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Employee(models.Model):
+    employee_id  = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    desgination = models.CharField(max_length=100)
+    supervisor = models.ForeignKey('Employee', null=True, blank=True)
+    class Meta:
+        db_table = 'tbl_employee'
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.full_name, self.employee_id)
